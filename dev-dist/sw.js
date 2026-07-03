6fce1d8bdd483db1624f6ef27da1d791ccc6b417
Romeoraffael <romeoraffael24@gmail.com>
2026-07-03 19:55:09 +0700

    Imporvementttt

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hl94qv5m3bg"
+    "revision": "0.i299qe4r5cg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
